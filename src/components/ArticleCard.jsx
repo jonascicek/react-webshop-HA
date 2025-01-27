9c3089f863bb6f76f450895f26fa539ab153bbbd
jonascicek <jonascicek98@gmail.com>
2025-01-27 15:55:53 +0100

    Added Availability

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,8 +4,11 @@ function ArticleCard(props) {
         <img src={props.article.thumbnail} alt={props.article.name} />
         <h3>{props.article.name}</h3>
         <span>Price: {props.article.price} </span>
-        <span>Availability: {props.article.available}</span>
+        <span className="flex items-center"> 
+        Available: {props.article.available ? (<img src="src/assets/square-check.png" alt="Available" width={18} height={18}></img>) : (<img src="src/assets/package-x.png" alt="Not Available" width={18} height={18}></img>)}
+        </span>
       </div>
     );
   }
+
   export default ArticleCard;
